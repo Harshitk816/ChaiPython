e7fa492cc888a2df1cd0ec714fcb4df465348ae7
Harshitk816 <harshitkaushal816@gmail.com>
2024-09-15 22:40:22 +0530

    Added comments to code

---
+++ 01_basics/chai.py
@@ -0,0 +1,3 @@
+from hello_chai import chai  #importing fucntion from another .py file
+
+chai("Ginger Tea")
--- 01_basics/hello_chai.py
+++ 01_basics/hello_chai.py
@@ -1,6 +1,6 @@
-# print("chai aur python")
+# print("chai aur python")  printing the output
 
-def chai(n):
+def chai(n):        #function definition
     print(n)
 
-chai('Hello')
+chai('Hello')       #function calling
